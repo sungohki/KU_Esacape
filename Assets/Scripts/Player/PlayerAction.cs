1e459ed43543b4edffcfcdc0bda4e7bb3db09843
sungohki <sokim1201@naver.com>
2023-10-14 16:34:04 +0900

    modified: add contents on funcs

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -6,15 +6,32 @@ public class PlayerAction : MonoBehaviour
 {
 
     // Attack Func
-    void PlayerAttack() {}
+    void PlayerAttack() {
+        // 공격키를 입력하는 경우
+        if(Input.GetKey(KeyCode.K)) {
+            // 공격 모션 적용
+
+            // playerAttackRange 내에 NPC 객체가 있는 경우
+            
+        }
+    }
 
     // Damage Func
-    void PlayerDamaged() {}
+    void PlayerDamaged() {
+        // playerDamageRange 내에 NPC 객체가 있는 경우
+        if (true) {
+            PlayerSoundOccur();
+        }
+    }
 
     // Sound Occur Func
-    void PlayerSoundOccur() {}
+    void PlayerSoundOccur() {
+        // playerSoundRange 범위의 영역 생성
+    }
 
     // Interaction Obj func
-    void PlayerInteraction() {}
+    void PlayerInteraction() {
+        if (Input.GetKey(KeyCode.J)) {}
+    }
 
 }
