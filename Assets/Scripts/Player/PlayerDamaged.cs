d54b4063868e41b60e44d60c382c5e599d014784
sungohki <sokim1201@naver.com>
2023-10-22 22:06:20 +0900

    modified: modify contents

--- Assets/Scripts/Player/PlayerDamaged.cs
+++ Assets/Scripts/Player/PlayerDamaged.cs
@@ -5,17 +5,37 @@ public class PlayerDamaged : MonoBehaviour
     PlayerStat playerStat = new PlayerStat();
 
     private int playerLife;
-    void Start() {
+    private bool isInvulnerable;
+    private Renderer playerRenderer;
+    private void Start() {
         playerLife = playerStat.getPlayerLife();
         if (playerLife < 1)
             playerLife = 3;
+        isInvulnerable = false;
+        playerRenderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        if (isInvulnerable)
+            playerRenderer.enabled = !playerRenderer.enabled;
     }
 
     private void OnCollisionEnter(Collision other) {
         // Collision with DamageRange and NPC obj
-        if (true && playerLife >= 0) {
+        if (other.gameObject.CompareTag("NPC") && playerLife >= 0) {
+            // 1. reduce hp
             playerLife--;
             Debug.Log($"Player Life : {playerLife}");   // Test output
+
+            // 2. toggle Invulnerablity
+            isInvulnerable = true;
+            Invoke("toggleInvulnerable", 3.0f);     // Invulnerate for 3.0 seconde
         }
+    }
+
+    private void toggleInvulnerable() {
+        isInvulnerable = false;
+        playerRenderer.enabled = true;
     }
 }
--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    string destination;
+
     private void OnCollisionStay(Collision other) {
-        if (other.gameObject.tag == "door") {
+        if (other.gameObject.tag == "Door") {
             if (Input.GetKey(KeyCode.F)) {
                 // TODO: Change The Scene
+                SceneManager.LoadScene(other.gameObject.name);
             }
         }
     }
