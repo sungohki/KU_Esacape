5eb01ca491c141f1eddc49c3650d8f79de1a4d4c
sungohki <sokim1201@naver.com>
2023-10-22 22:15:44 +0900

    modified: modify

--- Assets/Scripts/Player/PlayerDamaged.cs
+++ Assets/Scripts/Player/PlayerDamaged.cs
@@ -7,30 +7,33 @@ public class PlayerDamaged : MonoBehaviour
     private int playerLife;
     private bool isInvulnerable;
     private Renderer playerRenderer;
+    private GameObject player;
     private void Start() {
+        player = GameObject.Find("Player");
         playerLife = playerStat.getPlayerLife();
         if (playerLife < 1)
             playerLife = 3;
         isInvulnerable = false;
-        playerRenderer = GetComponent<Renderer>();
+        playerRenderer = player.GetComponent<Renderer>();
     }
 
     private void Update()
     {
         if (isInvulnerable)
+            // Blinking Effect 
             playerRenderer.enabled = !playerRenderer.enabled;
     }
 
     private void OnCollisionEnter(Collision other) {
         // Collision with DamageRange and NPC obj
-        if (other.gameObject.CompareTag("NPC") && playerLife >= 0) {
+        if (other.gameObject.CompareTag("NPC")) {
             // 1. reduce hp
             playerLife--;
             Debug.Log($"Player Life : {playerLife}");   // Test output
 
             // 2. toggle Invulnerablity
             isInvulnerable = true;
-            Invoke("toggleInvulnerable", 3.0f);     // Invulnerate for 3.0 seconde
+            Invoke("toggleInvulnerable", 3.0f);     // Invulnerate for 3.0 seconds
         }
     }
 
