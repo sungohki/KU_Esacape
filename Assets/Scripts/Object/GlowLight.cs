49381ef843bcd9cb68e5b344dffdfe51fdad0701
bunzen1012 <107540683+bunzen1012@users.noreply.github.com>
2023-12-06 04:38:18 +0900

    fubv

--- Assets/Scripts/Object/GlowLight.cs
+++ Assets/Scripts/Object/GlowLight.cs
@@ -6,7 +6,7 @@ public class GlowLight : MonoBehaviour
 {
     public Color lightColor = Color.green; // 전등의 색상
     public float lightRange = 10f; // 전등의 범위
-    public float lightIntensity = 1f; // 전등의 강도
+    public float lightIntensity = 10f; // 전등의 강도
 
     void Start()
     {
