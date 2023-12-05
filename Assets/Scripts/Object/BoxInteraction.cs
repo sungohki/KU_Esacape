293c19a8bdec5a44ce935beec62b1aaee1960884
bunzen1012 <107540683+bunzen1012@users.noreply.github.com>
2023-12-06 03:58:46 +0900

    s
    s

--- Assets/Scripts/Object/BoxInteraction.cs
+++ Assets/Scripts/Object/BoxInteraction.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BoxInteraction : MonoBehaviour
 {
     public GameObject floorKey;
+    public AudioClip brokesound;
 
     void ontriggerenter(Collider other) {
         if (other.CompareTag("weapon")) {
@@ -11,6 +12,11 @@ public class BoxInteraction : MonoBehaviour
     }
     private void OnDestroy()
     {
+        if (brokesound != null)
+        {
+            SoundManager.instance.PlaySound(brokesound);
+        }
+
         if (floorKey != null) {
             Instantiate(floorKey, transform.position, transform.rotation);
         }
--- Assets/Scripts/Object/KeyInteraction.cs
+++ Assets/Scripts/Object/KeyInteraction.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class KeyInteraction : MonoBehaviour
 {
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider
+        other) {
         if (other.gameObject.CompareTag("Player")) {
             Debug.Log("Info: get a key!");
             PlayerPrefs.SetInt("hasKey", 1);
             other.gameObject.GetComponent<PlayerStat>().setHasKey(1);
             Destroy(gameObject);
+        }
+    }
+
+    public AudioClip getSound; // 파괴 시 재생할 사운드
+
+    void OnDestroy()
+    {
+        if (getSound != null)
+        {
+            SoundManager.instance.PlaySound(getSound);
         }
     }
 }
